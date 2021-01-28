ebe3eca97d3891b5f7d9bfa32bc4c774f58bc19c
Henrique Atanásio Wegner <43445677+HenriqueWegner@users.noreply.github.com>
2021-01-28 16:58:27 +0000

    First Commit
    - Menu working.
    - Modify page can call API and get the full table.
    - TBO page can call API and get items where quantity is equals or under the threshold.

---
+++ Stock Control Web App - Front End/vendor/bootstrap/js/getall.js
@@ -0,0 +1,51 @@
+// api url 
+const api_url =  
+      "http://127.0.0.1:5000/API/resources/items/all"; 
+  
+// Defining async function 
+async function getapi(url) { 
+    
+    // Storing response 
+    const response = await fetch(url); 
+    
+    // Storing data in form of JSON 
+    var data = await response.json(); 
+    console.log(data); 
+    if (response) { 
+        hideloader(); 
+    } 
+    show(data);
+    return(data); 
+} 
+// Calling that async function 
+getapi(api_url); 
+
+
+// Function to hide the loader 
+function hideloader() { 
+    document.getElementById('loading').style.display = 'none'; 
+}
+
+function show(data) { 
+    let tab =  
+        `<tr> 
+          <th>ID</th> 
+          <th>NAME</th> 
+          <th>QUANTITY</th> 
+          <th>THRESHOLD</th>
+          <th>Type</th>  
+         </tr>`; 
+    
+    // Loop to access all rows  
+    for (let r of data) { 
+        tab += `<tr>  
+    <td>${r.ID} </td> 
+    <td>${r.name}</td> 
+    <td>${r.qty}</td>  
+    <td>${r.threshold}</td>
+    <td>${r.type}</td>            
+</tr>`; 
+    } 
+    // Setting innerHTML as tab variable 
+    document.getElementById("items").innerHTML = tab; 
+}  
